370861bc1c57b7abbba11031f892451c0ac1f17a
Jason Sonnichsen <Harliquinn@gmail.com>
2020-07-20 18:33:36 -0400

    Class created, test not passing

---
+++ gigasecond/gigasecond.ts
@@ -0,0 +1,10 @@
+export default class Gigasecond {
+
+    constructor(private currentDate:Date){    
+    }
+    date(){
+        return new Date(this.currentDate.getTime()+(10^9*1000));
+    }
+}
+
+ 
