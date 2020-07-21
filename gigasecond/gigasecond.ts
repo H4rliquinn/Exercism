721a8a65a8946f5a99d24832e56d4dab15c1305e
Jason Sonnichsen <Harliquinn@gmail.com>
2020-07-21 14:49:00 -0400

    No compiler  necessary apparently. 1 Test Passed.

--- gigasecond/gigasecond.ts
+++ gigasecond/gigasecond.ts
@@ -3,8 +3,7 @@ export default class Gigasecond {
     constructor(private currentDate:Date){    
     }
     date(){
-        return new Date(this.currentDate.getTime()+(10^9*1000));
+        console.log("TEST",this.currentDate,this.currentDate.getTime());
+        return new Date(this.currentDate.getTime()+((10**9)*1000));//
     }
 }
-
- 
