a62b0d96a9af89a80e31545be017f19e33dc137d
Jason Sonnichsen <Harliquinn@gmail.com>
2020-07-21 14:51:47 -0400

    All tests passed

--- gigasecond/gigasecond.ts
+++ gigasecond/gigasecond.ts
@@ -3,7 +3,6 @@ export default class Gigasecond {
     constructor(private currentDate:Date){    
     }
     date(){
-        console.log("TEST",this.currentDate,this.currentDate.getTime());
         return new Date(this.currentDate.getTime()+((10**9)*1000));//
     }
 }
