72304b3c4c145f8628b562d58f1e9ae2e7b57aad
Jason Sonnichsen <Harliquinn@gmail.com>
2020-07-27 16:10:46 -0400

    Basic class laid out. Edge Case tests passing.

---
+++ run-length-encoding/run-length-encoding.ts
@@ -0,0 +1,18 @@
+class RunLengthEncoding{
+    public static encode(str:string):string{
+        if (str.length==0){
+            return str;
+        }
+
+        return str;
+    }
+    public static decode(str:string):string{
+        if (str.length==0){
+            return str;
+        }
+
+        return str;        
+    }
+}
+
+export default RunLengthEncoding;
