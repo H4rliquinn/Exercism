0eddf5bd0da1625c1b86eb285ff315802c3dbb51
Jason Sonnichsen <Harliquinn@gmail.com>
2020-07-27 15:57:32 -0400

    Updated two-fer based on mentor feedback.

--- two-fer/two-fer.ts
+++ two-fer/two-fer.ts
@@ -1,12 +1,6 @@
 class TwoFer {
-  public static twoFer(a?:string): string {
-    let retVal:string;
-    let name:string='you';
-    if (a){
-      name=a;
-    }
-    retVal=`One for ${name}, one for me.`;
-    return retVal;
+  public static twoFer(name:string='you'): string {
+    return `One for ${name}, one for me.`;
   }
 }
 export default TwoFer
