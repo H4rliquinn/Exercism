8bf6ab330f927788397438958d431340b8e36203
Jason Sonnichsen <Harliquinn@gmail.com>
2020-07-31 12:52:53 -0400

    Passed first test

--- resistor-color-duo/resistor-color-duo.ts
+++ resistor-color-duo/resistor-color-duo.ts
@@ -1,8 +1,12 @@
 export class ResistorColor {
   private colors: string[];
+  private lookup:{[key:string]:string}={black:"0",brown:"1",red:"2",orange:"3",yellow:"4",
+      green:"5",blue:"6",violet:"7",grey:"8",white:"9"};
 
   constructor(colors: string[]) {
     this.colors = colors;
   }
-  value = (): number => 0;
+  value = (): number => {
+    return parseInt(this.lookup[this.colors[0]]+this.lookup[this.colors[1]]);
+  }
 }
