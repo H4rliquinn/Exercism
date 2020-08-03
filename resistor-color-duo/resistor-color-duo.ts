b5abc4d1c90064ac5767eb5e5c96f8cc32a5dbe5
Jason Sonnichsen <Harliquinn@gmail.com>
2020-08-03 13:04:14 -0400

    Updated resistor duo based on mentor comments

--- resistor-color-duo/resistor-color-duo.ts
+++ resistor-color-duo/resistor-color-duo.ts
@@ -1,9 +1,10 @@
+type Color = "black"|"brown"|"red"|"orange"|"yellow"|"green"|"blue"|"violet"|"grey"|"white";
 export class ResistorColor {
-  private colors: string[];
-  private lookup:{[key:string]:string}={black:"0",brown:"1",red:"2",orange:"3",yellow:"4",
+  private colors: Color[];
+  private lookup:Record<Color, string>={black:"0",brown:"1",red:"2",orange:"3",yellow:"4",
       green:"5",blue:"6",violet:"7",grey:"8",white:"9"};
 
-  constructor(colors: string[]) {
+  constructor(colors: Color[]) {
     if (colors.length<2){
       throw "At least two colors need to be present";
     } else{
