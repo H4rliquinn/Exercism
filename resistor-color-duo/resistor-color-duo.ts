7a5d46d1b66c583104e98d2899186368321c4c26
Jason Sonnichsen <Harliquinn@gmail.com>
2020-08-05 12:49:55 -0400

    Is this guy kidding me? Lined up lookup colors for easy reading. Am I learning TypeScript or learning formatting?

--- resistor-color-duo/resistor-color-duo.ts
+++ resistor-color-duo/resistor-color-duo.ts
@@ -1,8 +1,18 @@
 type Color = "black"|"brown"|"red"|"orange"|"yellow"|"green"|"blue"|"violet"|"grey"|"white";
 export class ResistorColor {
   private colors: Color[];
-  private lookup:Record<Color, number>={black:0,brown:1,red:2,orange:3,yellow:4,
-      green:5,blue:6,violet:7,grey:8,white:9};
+  private lookup:Record<Color, number>={
+    black:0,
+    brown:1,
+    red:2,
+    orange:3,
+    yellow:4,
+    green:5,
+    blue:6,
+    violet:7,
+    grey:8,
+    white:9
+  };
 
   constructor(colors: Color[]) {
     if (colors.length<2){
