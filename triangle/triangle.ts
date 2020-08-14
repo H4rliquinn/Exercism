9f8b2f56f475b3db3c6a7277f758afb76a0813f8
Jason Sonnichsen <Harliquinn@gmail.com>
2020-08-14 16:11:55 -0400

    Added triangle inequality throw All tests passed

--- triangle/triangle.ts
+++ triangle/triangle.ts
@@ -8,16 +8,17 @@ export default class Triangle {
     kind():string {
         if (this.sides.length!=3){
             throw "Invalid number of sides for a triangle";
-        } else if (this.sides[0]<=0||
-                    this.sides[1]<=0||
-                    this.sides[2]<=0){  
-            throw "All sides must be positive numbers";
         }
         let a,b,c:number;
         a=this.sides[0];
         b=this.sides[1];
         c=this.sides[2];
-
+        if (a<=0||b<=0||c<=0){  
+            throw "All sides must be positive numbers";
+        } else if (a+b<c||b+c<a||a+c<b){
+            throw "Triangle inequality violation"
+        }
+        
         if (a==b && b==c){
             return "equilateral";
         } else if (a==b || b==c || a==c){
