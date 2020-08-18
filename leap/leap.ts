d51bb50d8523c211bd9c69aa9d1f9b85db305db8
Jason Sonnichsen <Harliquinn@gmail.com>
2020-08-18 12:58:46 -0400

    Refactored leap again per mentor

--- leap/leap.ts
+++ leap/leap.ts
@@ -1,8 +1,4 @@
 function isLeapYear( year:number ):boolean {
-    if ((year%4==0 && year%100!=0) || (year%100==0 && year%400===0)){
-            return true;
-    }
-    return false;
+    return (year%4==0 && year%100!=0) || (year%100==0 && year%400===0);
 }
-
 export default isLeapYear
