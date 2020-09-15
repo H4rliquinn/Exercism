05a0d9a8383719eadf49448fe4a0ff8fb91853d6
Jason Sonnichsen <Harliquinn@gmail.com>
2020-09-15 13:29:29 -0400

    Added CODON type and Amino Acid lookup

--- protein-translation/protein-translation.ts
+++ protein-translation/protein-translation.ts
@@ -1,4 +1,19 @@
+type CODON = "AUG"|"UUU"|"UUC"|"UUA"|"UUG"|"UCU"|"UCC"|"UCA"|
+"UCG"|"UAU"|"UAC"|"UGU"|"UGC"|"UGG"|"UAA"|"UAG"|"UGA";
+
+let complements:Record<CODON,String>={
+    "AUG":"Methionine",
+    "UUU":"Phenylalanine","UUC":"Phenylalanine",
+    "UUA":"Leucine","UUG":"Leucine",
+    "UCU":"Serine","UCC":"Serine","UCA":"Serine","UCG":"Serine",
+    "UAU":"Tyrosine","UAC":"Tyrosine",
+    "UGU":"Cysteine","UGC":"Cysteine",
+    "UGG":"Tryptophan",
+    "UAA":"STOP","UAG":"STOP","UGA":"STOP"
+}
 class ProteinTranslation {
+
+
     static proteins( /* Parameters go here */ ) {
         // Your code here
     }
