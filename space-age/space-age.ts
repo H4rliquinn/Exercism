dbd51284190f28b4f185cfdc2d5ca1ceda3caa70
Jason Sonnichsen <Harliquinn@gmail.com>
2020-09-22 14:42:54 -0400

    Added Space Age Created class and methods

---
+++ space-age/space-age.ts
@@ -0,0 +1,34 @@
+
+
+class SpaceAge{
+    age=0;
+    constructor(age:number){
+        this.age=age;
+    }
+    onEarth():number{
+        return this.age;
+    }
+    onMercury():number{
+        return this.age;
+    }
+    onVenus():number{
+        return this.age;
+    }
+    onMars():number{
+        return this.age;
+    }
+    onJupiter():number{
+        return this.age;
+    }
+    onSaturn():number{
+        return this.age;
+    }
+    onUranus():number{
+        return this.age;
+    }
+    onNeptune():number{
+        return this.age;
+    }
+}
+
+export default SpaceAge
