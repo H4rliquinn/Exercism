a3b10edb259b2a6a7d6f233baeb4b2dfa6cc70b1
Jason Sonnichsen <Harliquinn@gmail.com>
2020-09-23 12:22:34 -0400

    Refactor to eliminate repditive  code

--- space-age/space-age.ts
+++ space-age/space-age.ts
@@ -3,33 +3,35 @@
 class SpaceAge{
     seconds=0;
     earthYear=31557600;
+    earthYearsInt=0;
     constructor(seconds:number){
         this.seconds=seconds;
+        this.earthYearsInt=this.seconds/this.earthYear*100;
     }
     onEarth():number{
-        return Math.round((this.seconds/this.earthYear*100))/100;
+        return Math.round((this.earthYearsInt))/100;
     }
     onMercury():number{
         
-        return Math.round((this.seconds/this.earthYear/0.2408467*100))/100;
+        return Math.round((this.earthYearsInt/0.2408467))/100;
     }
     onVenus():number{
-        return this.seconds;
+        return Math.round((this.earthYearsInt/0.2408467))/100;
     }
     onMars():number{
-        return this.seconds;
+        return Math.round((this.earthYearsInt/0.2408467))/100;
     }
     onJupiter():number{
-        return this.seconds;
+        return Math.round((this.earthYearsInt/0.2408467))/100;
     }
     onSaturn():number{
-        return this.seconds;
+        return Math.round((this.earthYearsInt/0.2408467))/100;
     }
     onUranus():number{
-        return this.seconds;
+        return Math.round((this.earthYearsInt/0.2408467))/100;
     }
     onNeptune():number{
-        return this.seconds;
+        return Math.round((this.earthYearsInt/0.2408467))/100;
     }
 }
 
